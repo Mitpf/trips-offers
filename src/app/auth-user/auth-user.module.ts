20850af16b265d1e5b1741ada610c93b42616f1c
Mitpf <113504721+Mitpf@users.noreply.github.com>
2024-03-29 18:14:00 +0200

    basic client side form validation of login input data

--- src/app/auth-user/auth-user.module.ts
+++ src/app/auth-user/auth-user.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import { MaxCountDirective } from './max-count.directive';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, RouterModule],
-  exports:[LoginComponent, RegisterComponent]
+  declarations: [LoginComponent, RegisterComponent, MaxCountDirective],
+  imports: [CommonModule, RouterModule, FormsModule],
+  exports: [LoginComponent, RegisterComponent],
 })
 export class AuthUserModule {}
--- src/app/auth-user/login/login.component.ts
+++ src/app/auth-user/login/login.component.ts
@@ -1,17 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit{
+ 
+  @ViewChild('email') emailModel!: NgModel;
+  @ViewChild('password') passwordModel!: NgModel;
+
+  isEmailFieldFocused: boolean = false;
+  isPasswordFieldFocused: boolean = false;
+
   constructor(private authService: AuthService) {}
 
-  login(email: string, password: string) {
+  ngAfterViewInit() {
+    if (!this.emailModel || !this.passwordModel) {
+      console.error('Email model or password model is not defined.');
+    }
+
+    if (!this.emailModel || !this.passwordModel) {
+      console.error('Email model or password model is not defined.');
+    } else {
+      console.log('Email model and password model are defined.');
+    }
+  }
+
+  formSubmitHandler(form: NgForm) {
+    console.log(form.value);
+
+    console.log('invalid', form.invalid);
+
+    if (form?.invalid) {
+      console.log('Form is invalid!');
+      return;
+    }
+
+    const { email, password } = form?.value;
+    //form.reset();
+    //form.setValue({ email: '', password: '' });
+
     this.authService.login(email, password);
   }
+
+  onFocus(field: string) {
+    if (field === 'email') {
+      this.isEmailFieldFocused = true;
+    } else if (field === 'password') {
+      this.isPasswordFieldFocused = true;
+    }
+  }
+
+  onBlur(field: string) {
+    if (field === 'email') {
+      this.isEmailFieldFocused = false;
+    } else if (field === 'password') {
+      this.isPasswordFieldFocused = false;
+    }
+  }
+
+  get emailErrorMessage(): string {
+    if (this.emailModel?.touched && !this.isEmailFieldFocused) {
+      if (this.emailModel.errors?.['required']) {
+        return 'Email is required!';
+      }
+      if (this.emailModel.errors?.['email']) {
+        return 'Email is not valid!';
+      }
+    }
+    return '';
+  }
+
+  get passwordErrorMessage(): string {
+    if (this.passwordModel?.touched && !this.isPasswordFieldFocused) {
+      if (this.passwordModel.errors?.['required']) {
+        return 'Password is required!';
+      }
+      if (this.passwordModel.errors?.['appMaxCount']) {
+        return `Password is less than ${this.passwordModel.errors?.['appMaxCount']} characters!`;
+      }
+    }
+    return '';
+  }
 }
-
-
