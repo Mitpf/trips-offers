ebb524b021de7cd776e9bea6bda66b1d07422ece
Mitpf <113504721+Mitpf@users.noreply.github.com>
2024-04-03 23:29:22 +0300

    some home page created

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from './../auth-user/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  
+  get isLoggedUser() {
+    return AuthService.isLoggedUser();
+  }
 
 }
