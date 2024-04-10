0d55df0bf1eb20f2d6057ef79d64720b31d749b1
Mitpf <113504721+Mitpf@users.noreply.github.com>
2024-04-10 17:46:58 +0300

    view details offer page works

--- src/app/offers/offer-details/offer-details.component.ts
+++ src/app/offers/offer-details/offer-details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs';
+import { OfferDB } from '../types';
+import { OffersService } from '../offer-services/offers.service';
+import { UtilService } from 'src/app/app-services-utils/util.service';
 
 @Component({
   selector: 'app-offer-details',
   templateUrl: './offer-details.component.html',
-  styleUrls: ['./offer-details.component.css']
+  styleUrls: ['./offer-details.component.css'],
 })
-export class OfferDetailsComponent {
+export class OfferDetailsComponent implements OnInit {
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private offerService: OffersService
+  ) {}
 
+  offer = {} as OfferDB;
+  loggedUserID: string = '';
+
+  ngOnInit(): void {
+    this.loggedUserID = UtilService.getUserData()?.userId;
+
+    this.activeRoute.params.subscribe((params) => {
+      const offerId = params['offerId'];
+
+      this.offerService.getOneOffer(offerId).subscribe((offerData: any) => {
+        this.offer = offerData;
+      });
+    });
+  }
 }
